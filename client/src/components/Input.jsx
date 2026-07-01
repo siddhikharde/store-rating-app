ec09eb711e94566514774581340038921404c2dc
Siddhi Kharde <siddhikharde2005@gamil.com>
2026-07-01 18:48:26 +0530

    Input component created

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
-function Input() {
+function Input({
+  type = "text",
+  name,
+  placeholder,
+  value,
+  onChange
+}) {
   return (
-    <div>
-      
-    </div>
-  )
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="w-full p-3 mb-4 rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-[#eebbc3]"
+    />
+  );
 }
 
-export default Input
+export default Input;
