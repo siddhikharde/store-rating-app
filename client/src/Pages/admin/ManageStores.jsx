cd62bea912533280f5f7ee98d137be28311fb3a0
Siddhi Kharde <siddhikharde2005@gamil.com>
2026-07-01 18:18:59 +0530

    folder structure created

---
+++ client/src/Pages/admin/ManageStores.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ManageStores() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ManageStores
---
+++ client/src/Pages/user/StoreList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StoreList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StoreList
