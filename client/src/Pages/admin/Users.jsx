7f79f9238c08958b2266180135f94019d0f8b9df
Siddhi Kharde <siddhikharde2005@gamil.com>
2026-07-04 17:56:58 +0530

    earch user functionality added

--- client/src/Pages/admin/Users.jsx
+++ client/src/Pages/admin/Users.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import DashboardLayout from "../../layouts/DashboardLayout";
 import api from "../../api/axios";
 import toast from "react-hot-toast";
+import Input from "../../components/Input";
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetchUsers();
@@ -22,13 +24,43 @@ function Users() {
     }
   };
 
+
+  const handleSearch = async (e) => {
+  const value = e.target.value;
+
+  setSearch(value);
+
+  try {
+    const response = await api.get(
+      `/admin/users/search?search=${value}`
+    );
+
+    setUsers(response.data);
+  } catch (error) {
+    toast.error("Search Failed");
+  }
+};
   return (
     <DashboardLayout>
-      <h1 className="text-3xl font-bold mb-6">
-        Users
-      </h1>
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-6">
+
+  <h1 className="text-3xl font-bold text-[#232946]">
+    Users
+  </h1>
+
+  <div className="w-full md:w-80">
+    <Input
+      type="text"
+      placeholder="Search users..."
+      value={search}
+      onChange={handleSearch}
+    />
+  </div>
+
+</div>
 
       <div className="bg-white rounded-xl shadow overflow-x-auto">
+        
         <table className="w-full">
 
           <thead className="bg-[#232946] text-white">
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,19 +1,18 @@
 import { FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Sidebar({ isOpen, setIsOpen }) {
   return (
     <>
       <div
-        className={`fixed inset-0 bg-black/40 z-40 md:hidden ${
-          isOpen ? "block" : "hidden"
-        }`}
+        className={`fixed inset-0 bg-black/40 z-40 md:hidden ${isOpen ? "block" : "hidden"
+          }`}
         onClick={() => setIsOpen(false)}
       />
 
       <aside
-        className={`fixed md:static top-0 left-0 z-50 h-screen w-64 bg-[#232946] text-white p-6 transform transition-transform duration-300 ${
-          isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
-        }`}
+        className={`fixed md:static top-0 left-0 z-50 h-screen w-64 bg-[#232946] text-white p-6 transform transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
+          }`}
       >
         <button
           onClick={() => setIsOpen(false)}
@@ -28,7 +27,9 @@ function Sidebar({ isOpen, setIsOpen }) {
 
         <ul className="space-y-6">
           <li>Dashboard</li>
-          <li>Users</li>
+          <Link to="/admin/users">
+            <li className="flex items-center gap-3 hover:text-[#eebbc3]"> Users</li>
+          </Link>
           <li>Stores</li>
         </ul>
       </aside>
